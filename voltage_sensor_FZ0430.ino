f96c4f5ac2917ead83819df989c19c1c25e225fb
Roberto Toapanta <robertocarlos.toapanta@gmail.com>
2020-08-19 11:35:56 -0500

    comment

--- voltage_sensor_FZ0430.ino
+++ voltage_sensor_FZ0430.ino
@@ -37,8 +37,8 @@ void loop() {
   delay(500);
 }
  
-// cambio de escala entre floats
-float fmap(float x, float in_min, float in_max, float out_min, float out_max)
-{
-   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
+// cambio de escala entre floats - prueba
+//float fmap(float x, float in_min, float in_max, float out_min, float out_max)
+//{
+//   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+//}
